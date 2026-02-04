f7a99a1269bfd60b9a2c8ac0f426a681ade79bc6
Takamasa <tsbeatbox193@gmail.com>
2026-02-04 11:08:32 +0900

    keymaps updated, should be prettierd

--- keyboards/crkbd/keymaps/takamasa1999/keymap.c
+++ keyboards/crkbd/keymaps/takamasa1999/keymap.c
@@ -11,15 +11,19 @@ void kill_caps_lock(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case M0:
-            register_mods(MOD_BIT(KC_LGUI));
-            tap_code(KC_TAB);
-            unregister_mods(MOD_BIT(KC_LGUI));
+            if (record->event.pressed) {
+                register_mods(MOD_BIT(KC_LGUI));
+                tap_code(KC_TAB);
+                unregister_mods(MOD_BIT(KC_LGUI));
+            }
             break;
 
         case M1:
-            register_mods(MOD_BIT(KC_LGUI) | MOD_BIT(KC_LSFT));
-            tap_code(KC_TAB);
-            unregister_mods(MOD_BIT(KC_LGUI) | MOD_BIT(KC_LSFT));
+            if (record->event.pressed) {
+                register_mods(MOD_BIT(KC_LGUI) | MOD_BIT(KC_LSFT));
+                tap_code(KC_TAB);
+                unregister_mods(MOD_BIT(KC_LGUI) | MOD_BIT(KC_LSFT));
+            }
             break;
 
         case M2:
@@ -69,71 +73,66 @@ bool led_update_user(led_t led_state) {
     return true;
 }
 
-const uint16_t PROGMEM
-    keymaps[][MATRIX_ROWS][MATRIX_COLS] =
-        {
-            [0] =
-                {
-                    {M3, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_VOLU},
-                    {M2, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_VOLD},
-                    {M1, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, M5, LT(1, KC_TAB), LT(3, KC_SPC), KC_NO},
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] =
+                                                                  {
+                                                                      {M3, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_VOLU},
+                                                                      {M2, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_VOLD},
+                                                                      {M1, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO},
+                                                                      {KC_NO, KC_NO, KC_NO, M5, LT(1, KC_TAB), LT(3, KC_SPC), KC_NO},
 
-                    {KC_DEL, KC_P, KC_O, KC_I, KC_U, KC_Y, KC_BRIU},
-                    {KC_QUOT, RCTL_T(KC_SCLN), RALT_T(KC_L), RGUI_T(KC_K), RSFT_T(KC_J), KC_H, KC_BRID},
-                    {M0, KC_SLSH, KC_DOT, KC_COMM, KC_M, KC_N, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, M4, LT(2, KC_BSPC), LT(4, KC_ENT), KC_NO},
-                },
+                                                                      {KC_DEL, KC_P, KC_O, KC_I, KC_U, KC_Y, KC_BRIU},
+                                                                      {KC_QUOT, RCTL_T(KC_SCLN), RALT_T(KC_L), RGUI_T(KC_K), RSFT_T(KC_J), KC_H, KC_BRID},
+                                                                      {M0, KC_SLSH, KC_DOT, KC_COMM, KC_M, KC_N, KC_NO},
+                                                                      {KC_NO, KC_NO, KC_NO, M4, LT(2, KC_BSPC), LT(4, KC_ENT), KC_NO},
+                                                                  },
 
-            [1] =
-                {
-                    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-                    {KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS},
-                    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                              [1] =
+                                                                  {
+                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+                                                                      {KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS},
+                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
 
-                    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-                    {KC_BSLS, KC_RBRC, KC_LBRC, KC_EQL, KC_MINS, KC_TRNS, KC_TRNS},
-                    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                },
+                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+                                                                      {KC_BSLS, KC_RBRC, KC_LBRC, KC_EQL, KC_MINS, KC_TRNS, KC_TRNS},
+                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                                  },
 
-            [2] =
-                {
-                    {KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS},
-                    {KC_GRV, LCTL_T(KC_1), LALT_T(KC_2), LGUI_T(KC_3), LSFT_T(KC_4), KC_5, KC_TRNS},
-                    {KC_TRNS, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                              [2] =
+                                                                  {
+                                                                      {KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS},
+                                                                      {KC_GRV, LCTL_T(KC_1), LALT_T(KC_2), LGUI_T(KC_3), LSFT_T(KC_4), KC_5, KC_TRNS},
+                                                                      {KC_TRNS, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
 
-                    {KC_TRNS, KC_F10, KC_F9, KC_F8, KC_F7, KC_F6, KC_TRNS},
-                    {KC_TRNS, RCTL_T(KC_0), RALT_T(KC_9), RGUI_T(KC_8), RSFT_T(KC_7), KC_6, KC_TRNS},
-                    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO},
-                },
+                                                                      {KC_TRNS, KC_F10, KC_F9, KC_F8, KC_F7, KC_F6, KC_TRNS},
+                                                                      {KC_TRNS, RCTL_T(KC_0), RALT_T(KC_9), RGUI_T(KC_8), RSFT_T(KC_7), KC_6, KC_TRNS},
+                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                                      {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO},
+                                                                  },
 
-            [3] =
-                {
-                    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-                    {KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS},
-                    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                              [3] =
+                                                                  {
+                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+                                                                      {KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS},
+                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
 
-                    {KC_TRNS, KC_TRNS, KC_END, KC_PAGE_UP, KC_PAGE_DOWN, KC_HOME, KC_TRNS},
-                    {KC_TRNS, KC_TRNS, KC_RGHT, KC_UP, KC_DOWN, KC_LEFT, KC_TRNS},
-                    {QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                },
+                                                                      {KC_TRNS, KC_TRNS, KC_END, KC_PAGE_UP, KC_PAGE_DOWN, KC_HOME, KC_TRNS},
+                                                                      {KC_TRNS, KC_TRNS, KC_RGHT, KC_UP, KC_DOWN, KC_LEFT, KC_TRNS},
+                                                                      {QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+                                                                  },
 
-            [4] =
-                {
-                    {KC_NO, KC_NO, KC_NO, KC_NO, QK_REBOOT, KC_NO, KC_NO},
-                    {KC_NO, RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, KC_NO, KC_NO},
-                    {QK_BOOT, RM_PREV, RM_HUED, RM_SATD, RM_VALD, KC_NO, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
+                                                              [4] = {
+                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, QK_REBOOT, KC_NO, KC_NO},
+                                                                  {KC_NO, RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, KC_NO, KC_NO},
+                                                                  {QK_BOOT, RM_PREV, RM_HUED, RM_SATD, RM_VALD, KC_NO, KC_NO},
+                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
 
-                    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-                    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-                }
-};
+                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
+                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
+                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
+                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
+                                                              }};

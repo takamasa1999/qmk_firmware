670d7cf37271a3c65ce9d4f338c589164722f876
Takamasa Sato <takamasa.sato@systemi.co.jp>
2026-06-30 02:31:37 +0900

    organize keymap

--- keyboards/crkbd/keymaps/takamasa1999/keymap.c
+++ keyboards/crkbd/keymaps/takamasa1999/keymap.c
@@ -1,31 +1,25 @@
 #include QMK_KEYBOARD_H
 
-enum custom_keycodes { M0 = SAFE_RANGE, M1, M2, M3, M4, M5 };
+// ============================================================
+// Custom keycodes
+// ============================================================
+enum custom_keycodes { M2 = SAFE_RANGE, M3, M4, M5 };
 
+// ============================================================
+// Helpers
+// ============================================================
 void kill_caps_lock(void) {
     if (host_keyboard_led_state().caps_lock) {
         tap_code(KC_CAPS);
     }
 }
 
+// ============================================================
+// Key processing
+// ============================================================
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case M0:
-            if (record->event.pressed) {
-                register_mods(MOD_BIT(KC_LGUI));
-                tap_code(KC_TAB);
-                unregister_mods(MOD_BIT(KC_LGUI));
-            }
-            break;
-
-        case M1:
-            if (record->event.pressed) {
-                register_mods(MOD_BIT(KC_LGUI) | MOD_BIT(KC_LSFT));
-                tap_code(KC_TAB);
-                unregister_mods(MOD_BIT(KC_LGUI) | MOD_BIT(KC_LSFT));
-            }
-            break;
-
+        // M2: press=CapsLock off  / release=ESC + Eisu
         case M2:
             if (record->event.pressed) {
                 kill_caps_lock();
@@ -34,7 +28,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(KC_LNG2);
             }
             break;
-
+        // M3: press=Eisu          / release=CapsLock on
         case M3:
             if (record->event.pressed) {
                 tap_code(KC_LNG2);
@@ -42,7 +36,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(KC_CAPS);
             }
             break;
-
+        // M4: press=CapsLock off  / release=Kana
         case M4:
             if (record->event.pressed) {
                 kill_caps_lock();
@@ -50,7 +44,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(KC_LNG1);
             }
             break;
-
+        // M5: press=CapsLock off  / release=Eisu
         case M5:
             if (record->event.pressed) {
                 kill_caps_lock();
@@ -62,6 +56,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+// ============================================================
+// LED: RGB on when CapsLock active
+// ============================================================
 bool led_update_user(led_t led_state) {
 #ifdef RGB_MATRIX_ENABLE
     if (led_state.caps_lock) {
@@ -73,66 +70,113 @@ bool led_update_user(led_t led_state) {
     return true;
 }
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] =
-                                                                  {
-                                                                      {M3, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_VOLU},
-                                                                      {M2, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_VOLD},
-                                                                      {M1, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO},
-                                                                      {KC_NO, KC_NO, KC_NO, M5, LT(1, KC_TAB), LT(3, KC_SPC), KC_NO},
+// ============================================================
+// Keymap  (LAYOUT_split_3x6_3_ex2)
+// ============================================================
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-                                                                      {KC_DEL, KC_P, KC_O, KC_I, KC_U, KC_Y, KC_BRIU},
-                                                                      {KC_QUOT, RCTL_T(KC_SCLN), RALT_T(KC_L), RGUI_T(KC_K), RSFT_T(KC_J), KC_H, KC_BRID},
-                                                                      {M0, KC_SLSH, KC_DOT, KC_COMM, KC_M, KC_N, KC_NO},
-                                                                      {KC_NO, KC_NO, KC_NO, M4, LT(2, KC_BSPC), LT(4, KC_ENT), KC_NO},
-                                                                  },
+    // ----------------------------------------------------------
+    // Layer 0: Base (Homerow mods: CTRL/ALT/GUI/SHIFT)
+    // ----------------------------------------------------------
+    [0] = LAYOUT_split_3x6_3_ex2(
+        // Left Top
+        M3, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_VOLU,
+        // Right Top
+        KC_BRIU, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
+        // Left Middle
+        M2, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_VOLD,
+        // Right Middle
+        KC_BRID, KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
+        // Left Bottom
+        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,
+        // Right Bottom
+        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
+        // Left Thumb
+        M5, LT(1, KC_TAB), LT(3, KC_SPC),
+        // Right Thumb
+        LT(4, KC_ENT), LT(2, KC_BSPC), M4),
 
-                                                              [1] =
-                                                                  {
-                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-                                                                      {KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS},
-                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+    // ----------------------------------------------------------
+    // Layer 1: Left symbols (raised by TAB)
+    // ----------------------------------------------------------
+    [1] = LAYOUT_split_3x6_3_ex2(
+        // Left Top
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Right Top
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Left Middle
+        KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS,
+        // Right Middle
+        KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+        // Left Bottom
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Right Bottom
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Left Thumb
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        // Right Thumb
+        KC_TRNS, KC_TRNS, KC_TRNS),
 
-                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-                                                                      {KC_BSLS, KC_RBRC, KC_LBRC, KC_EQL, KC_MINS, KC_TRNS, KC_TRNS},
-                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                                                                  },
+    // ----------------------------------------------------------
+    // Layer 2: Numbers / F-keys (raised by BSPC)
+    // ----------------------------------------------------------
+    [2] = LAYOUT_split_3x6_3_ex2(
+        // Left Top
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS,
+        // Right Top
+        KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS,
+        // Left Middle
+        KC_GRV, LCTL_T(KC_1), LALT_T(KC_2), LGUI_T(KC_3), LSFT_T(KC_4), KC_5, KC_TRNS,
+        // Right Middle
+        KC_TRNS, KC_6, RSFT_T(KC_7), RGUI_T(KC_8), RALT_T(KC_9), RCTL_T(KC_0), KC_TRNS,
+        // Left Bottom
+        KC_TRNS, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Right Bottom
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Left Thumb
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        // Right Thumb
+        KC_TRNS, KC_NO, KC_NO),
 
-                                                              [2] =
-                                                                  {
-                                                                      {KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS},
-                                                                      {KC_GRV, LCTL_T(KC_1), LALT_T(KC_2), LGUI_T(KC_3), LSFT_T(KC_4), KC_5, KC_TRNS},
-                                                                      {KC_TRNS, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
+    // ----------------------------------------------------------
+    // Layer 3: Navigation (raised by SPC)
+    // ----------------------------------------------------------
+    [3] = LAYOUT_split_3x6_3_ex2(
+        // Left Top
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Right Top
+        KC_TRNS, KC_HOME, KC_PAGE_DOWN, KC_PAGE_UP, KC_END, KC_TRNS, KC_TRNS,
+        // Left Middle
+        KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS,
+        // Right Middle
+        KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS,
+        // Left Bottom
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Right Bottom
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // Left Thumb
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        // Right Thumb
+        KC_TRNS, KC_TRNS, KC_TRNS),
 
-                                                                      {KC_TRNS, KC_F10, KC_F9, KC_F8, KC_F7, KC_F6, KC_TRNS},
-                                                                      {KC_TRNS, RCTL_T(KC_0), RALT_T(KC_9), RGUI_T(KC_8), RSFT_T(KC_7), KC_6, KC_TRNS},
-                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                                                                      {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO},
-                                                                  },
-
-                                                              [3] =
-                                                                  {
-                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-                                                                      {KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS},
-                                                                      {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-
-                                                                      {KC_TRNS, KC_TRNS, KC_END, KC_PAGE_UP, KC_PAGE_DOWN, KC_HOME, KC_TRNS},
-                                                                      {KC_TRNS, KC_TRNS, KC_RGHT, KC_UP, KC_DOWN, KC_LEFT, KC_TRNS},
-                                                                      {QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                                                                      {KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO},
-                                                                  },
-
-                                                              [4] = {
-                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, QK_REBOOT, KC_NO, KC_NO},
-                                                                  {KC_NO, RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, KC_NO, KC_NO},
-                                                                  {QK_BOOT, RM_PREV, RM_HUED, RM_SATD, RM_VALD, KC_NO, KC_NO},
-                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-
-                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-                                                                  {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-                                                              }};
+    // ----------------------------------------------------------
+    // Layer 4: RGB / System (raised by ENT)
+    // ----------------------------------------------------------
+    [4] = LAYOUT_split_3x6_3_ex2(
+        // Left Top
+        KC_NO, KC_NO, KC_NO, KC_NO, QK_REBOOT, KC_NO, KC_NO,
+        // Right Top
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        // Left Middle
+        KC_NO, RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, KC_NO, KC_NO,
+        // Right Middle
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        // Left Bottom
+        KC_NO, RM_PREV, RM_HUED, RM_SATD, RM_VALD, QK_BOOT,
+        // Right Bottom
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        // Left Thumb
+        KC_NO, KC_NO, KC_NO,
+        // Right Thumb
+        KC_NO, KC_NO, KC_NO),
+};
